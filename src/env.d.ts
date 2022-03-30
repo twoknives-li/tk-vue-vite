6ecaeb04946374c2166bba28c6f017f33577bd09
Dpeng-li <lidongpeng@126.com>
2022-03-30 10:43:45 +0800

    ··

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,5 +4,13 @@ declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
+  interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string;
+    readonly DZ_API: string;
+    // 更多环境变量...
+  }
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
   export default component
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({mode})=>{
+  const envConfig = loadEnv(mode,'./env')
+  const result = {
+    plugins: [vue() ],
+    envDir: './env',
+    envPrefix: ['VITE_','DZ_'],
+    build:{
+      manifest: true,
+      outDir: `./dist/${mode}`
+    }
+  };
+  return result;
 })
